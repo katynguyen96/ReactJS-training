12bdd80d625e8b0073242deb489aeaadb8f827e6
katynguyen96 <56082798+katynguyen96@users.noreply.github.com>
2022-07-21 17:32:44 +0700

    implement render list product #4

--- src/Common/CardItem/index.js
+++ src/Common/CardItem/index.js
@@ -1,19 +1,28 @@
 import  {Wrapper, Image, ItemInfo, Name, Info, WrapperButton, Button} from './style'
+import {useStore} from '../../store'
+// import {useState} from 'react'
 
 function CardItem () {
+	const [state] = useStore()
+
 	return (
-		<Wrapper>
-			<Image src='https://cdn.tgdd.vn/Products/Images/42/230521/iphone-13-pro-thumb-600x600.jpg'/>
-			<ItemInfo>
-				<Name>SamSung</Name>
-				<Info>Price: 200$</Info>
-				<Info>Brand: SamSung</Info>
-			</ItemInfo>
-			<WrapperButton>
-				<Button edit><i className="fas fa-edit"></i></Button>
-				<Button><i className="fas fa-trash-alt"></i></Button>
-			</WrapperButton>
-		</Wrapper>
+		<>
+			{state.products.map((product, index)=>(
+				<Wrapper key={index}>
+					<Image src={product.productImg}/>
+					<ItemInfo>
+						<Name>{product.productName}</Name>
+						<Info>Price: {product.productPrice}$</Info>
+						<Info>Brand: {product.productBrand}</Info>
+					</ItemInfo>
+					<WrapperButton>
+						<Button edit><i className="fas fa-edit"></i></Button>
+						<Button><i className="fas fa-trash-alt"></i></Button>
+					</WrapperButton>
+				</Wrapper>
+			))}
+		</>
+		
 	)
 }
 

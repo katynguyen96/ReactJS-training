238a3e014b037df9772a08f534d8a5160cbc22d2
katynguyen96 <56082798+katynguyen96@users.noreply.github.com>
2022-08-29 09:51:45 +0700

    implement issue #62

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,11 @@ export const REMOVE_PRODUCT = 'remove_product'
 export const SEARCH_PRODUCT = 'search_product'
 export const FILTER_PRODUCT = 'filter_product'
 export const CLEAR_FILTER = 'clear_filter'
+export const KEY = 'products'
+
+export const brandOptions = [
+		{id:1, value: '', text:'Choose brand'},
+		{id:2, value:'samsung', text:'Samsung'},
+		{id:3, value:'iphone', text:'Iphone'},
+		{id:4, value:'vivo', text:'Vivo'},
+	]
